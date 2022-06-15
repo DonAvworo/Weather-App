094f6e7f4cc20c38effbb923618338eb71d77e70
Don Avworo <101209035+DonAvworo@users.noreply.github.com>
2022-06-15 15:26:33 +0100

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,31 +13,22 @@ let uvIndexDisplay = document.querySelector('.uv-index');
 
 
 //create a function to check for the city input
-function check4CityWeather() {
-    alert('Clicked! Checking for weather...');
-//get the weather data from the API website 
-fetch ('https://api.openweathermap.org/data/3.0/weather?q='+searchInput.value+'&appid=b34a7759c1f7ec1096a8d091ee38b500')
-// //enter promise
-.then(response => response.json())
-.then(data => console.log(data)) //print the data to the console for debugging
-.catch(error => alert(error)) //alert the error if there is one to the user, may not be necessary
+function check4CityWeather(e) {
+    e.preventDefault();
+    let city = searchInput.value;
+    if (city === '') {
+        alert('Please enter a city');
+    }
+    else { 
+        // console.log(city);
+       // getWeather(city);
+
+        alert('Clicked! Checking for weather...');
+    //get the weather data from the API website 
+    fetch ('https://api.openweathermap.org/data/2.5/weather?q='+searchInput.value+'&appid=b34a7759c1f7ec1096a8d091ee38b500')
+    //enter promise
+    .then(response => response.json())
+    .then(data => console.log(data)) //print the data to the console for debugging
+    .catch(error => alert(error)) //alert the error if there is one to the user, may not be necessary
+    } 
 };
-
-// assynccronous method to get the weather data
-// fetch ('https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&appid=b34a7759c1f7ec1096a8d091ee38b500')
-// .then ( async (response) => {
-//   // get json response here
-//   let data = await response.json();
-  
-//   if(response.status === 200){
-//    // Process data here
-//   }else{
-//    // Rest of status codes (400,500,303), can be handled here appropriately
-//   }
-
-// })
-// .catch((err) => {
-//     console.log(err);
-// })
-
-
